99f43b5fb228ab30cd0be1c17c122ff6f933b764
cheng <finallycheng@outlook.com>
2021-03-09 08:13:20 +0800

    项目的初始化

---
+++ src/components/common/tabbar/tabbarBtn.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "tabbarBtn"
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/common/tabbar/tabbarShell.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="tabbar_shell">
+    <div class="tabbar_btn">
+      <img src="@/assets/image/home/home.svg" alt="" />
+      <div>首页</div>
+    </div>
+    <div class="tabbar_btn">
+      <img src="@/assets/image/home/home.svg" alt="" />
+      <div>首页</div>
+    </div>
+    <div class="tabbar_btn">
+      <img src="@/assets/image/home/home.svg" alt="" />
+      <div>首页</div>
+    </div>
+    <div class="tabbar_btn">
+      <img src="@/assets/image/home/home.svg" alt="" />
+      <div>首页</div>
+    </div>
+    <div class="tabbar_btn">
+      <img src="@/assets/image/home/home.svg" alt="" />
+      <div>首页</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tabbarShell"
+};
+</script>
+
+<style scoped></style>
