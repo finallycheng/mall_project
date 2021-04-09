7d4e180bf93c0358940cba8cde9d2dcd6356f4c9
cheng <finallycheng@outlook.com>
2021-04-10 00:00:15 +0800

    new

--- src/views/good/good.vue
+++ src/views/good/good.vue
@@ -1,31 +1,59 @@
 <template>
-  <nav-box>
-    <template v-slot:nav-left>
-      <div class="btn back"></div>
-    </template>
-    <template v-slot:nav-center>
-      <div class="good_info">
-        <div
-          class="itmes"
-          v-for="(itme, index) in ['商品', '参数', '评价', '推荐']"
-          :key="index"
-        >
-          {{ itme }}
+  <div>
+    <nav-box>
+      <template v-slot:nav-left>
+        <div class="btn back" @click="back()"></div>
+      </template>
+      <template v-slot:nav-center>
+        <div class="good_info">
+          <div
+            class="itmes"
+            v-for="(itme, index) in ['商品', '参数', '评价', '推荐']"
+            :key="index"
+          >
+            {{ itme }}
+          </div>
         </div>
-      </div>
-    </template>
-    <template v-slot:nav-right>
-      <div class="btn more"></div>
-    </template>
-  </nav-box>
+      </template>
+      <template v-slot:nav-right>
+        <div class="btn more"></div>
+      </template>
+    </nav-box>
+    <scroll>
+      <slider-view></slider-view>
+    </scroll>
+  </div>
 </template>
 
 <script>
+import Scroll from "@/components/common/scroll/scroll.vue";
 import NavBox from "@/components/common/navbox/navBox.vue";
 
+import SliderView from "./childComps/sliderView.vue";
+
+import { gainGoodData } from "@/network/good.js";
+
 export default {
+  data() {
+    return {
+      sliderImage: null
+    };
+  },
+  methods: {
+    back() {
+      this.$router.back();
+    },
+    gainGoodData() {
+      console.log(this.$route.path);
+      gainGoodData({});
+      // this.sliderImage =
+    }
+  },
+  created() {},
   components: {
-    NavBox
+    NavBox,
+    Scroll,
+    SliderView
   }
 };
 </script>
